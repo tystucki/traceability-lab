07140c63fb518a1ae04adfd6e36f21af33f081ad
Ty Stucki <tystucki@Tys-MacBook-Pro.local>
2022-09-22 15:38:23 -0700

    dasf

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const suggestionsBtn = document.getElementById('suggestionsButton')
 
 const getSuggestion = () => {
-    axios.get("./api/controller.js/suggestion")
+    axios.get("./api/controller.js/getSuggestion")
     .then(res => {
         const data = res.data;
         alert(data)
