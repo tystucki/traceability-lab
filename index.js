340a5a6b5e01ad33269580ec920697ff89789fe5
Ty Stucki <tystucki@Tys-MacBook-Pro.local>
2022-09-22 15:45:40 -0700

    dsf

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const suggestionsBtn = document.getElementById('suggestionsButton')
 
+const {getSuggestion} = require('./controller');
+
+app.get("/api/suggestion", getSuggestion);
+
 const getSuggestion = () => {
-    axios.get("./api/controller.js/getSuggestion")
+    axios.get("./api//getSuggestion")
     .then(res => {
         const data = res.data;
         alert(data)
