4c709e20219aac6527c1e7111ccc687a7727b641
Ty Stucki <tystucki@Tys-MacBook-Pro.local>
2022-09-22 13:47:13 -0700

    another

--- server.js
+++ server.js
@@ -6,7 +6,27 @@ const cors = require('cors')
 app.use(express.json())
 app.use(cors())
 
-// include and ititialize the rollbar library with your access token
+// include and initialize the rollbar library with your access token
+var Rollbar = require('rollbar')
+var rollbar = new Rollbar({
+  accessToken: 'a28401c4a17c4e249761ddb3de6c5bb0',
+  captureUncaught: true,
+  captureUnhandledRejections: true,
+})
 
-// record a generic message and send it to rollbar
+// record a generic message and send it to Rollbar
+rollbar.log('Hello world!')
 
+const groceryList = ['Buns', 'Ground Beef', 'Lettuce', 'Cheese', 'Mayonnaise', 'Soda', 'Chips']
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'))
+})
+
+app.get('/api/groceryList', (req, res) => {
+    res.status(200).send(students)
+})
+
+app.post('/api/groceryList', (req, res) => {
+    let {}
+})
